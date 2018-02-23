98881ab3560e0d29db34b7de9c4d241d00a11ec4
priyanair99 <35268358+priyanair99@users.noreply.github.com>
2018-02-23 12:23:07 -0500

    Add files via upload

---
+++ DAY 4 customtable/DAY 4 customtable/menuitems.swift
@@ -0,0 +1,20 @@
+//
+//  menuitems.swift
+//  DAY 4 customtable
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class MenuItems {
+    let names:[String] =
+        ["Margaretta pizza","BBQ Chicken Pizza","Pepperoni pizza","Sausage pizza","seafood pizza","Sausage deep fish","veggie lover's deep dish","Cheese crosaint","Blueberry timbit","Mushroom deep Dish"]
+    
+    let prices:[Double] =
+        [7.9,11.23,8.45,8.45,12.75,5.36,8.23,4.23,6.56,7.50]
+    
+    let specials:[Bool] =
+        [false,true,false,false,true,false,false,true,true,false]
+}
