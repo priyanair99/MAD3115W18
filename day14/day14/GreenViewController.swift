eaec04a92ffa962d26713f5ab681635987861682
priyanair99 <35268358+priyanair99@users.noreply.github.com>
2018-03-09 12:16:22 -0500

    Add files via upload

---
+++ day14/day14/GreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GreenViewController.swift
+//  day14
+//
+//  Created by MacStudent on 2018-03-09.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+        // Do any additional setup after loading the view.
+  }
+
+
+
---
+++ day14/day14/RootUIViewController.swift
@@ -0,0 +1,93 @@
+//
+//  RootUIViewController.swift
+//  day14
+//
+//  Created by MacStudent on 2018-03-09.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class RootUIViewController: UIPageViewController {
+    
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController(color: "Green"),
+                self.newColoredViewController(color: "Pink"),
+                self.newColoredViewController(color: "blue")]
+    }()
+    
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: color)
+    }
+    
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self as? UIPageViewControllerDataSource
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+        // Do any additional setup after loading the view.
+
+        
+        override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension RootUIViewController : UIPageViewControllerDataSource
+{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+   
+    
+    
+}
+
+    
+
+
